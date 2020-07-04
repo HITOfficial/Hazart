6e251f6a3aaee86ba438432391c6049e780741e5
Tomasz Bochnak <tomaszbochnak00@gmail.com>
2020-07-04 22:56:59 +0200

    changes

--- public/js/script-front.js
+++ public/js/script-front.js
@@ -1,37 +1,19 @@
-var data
+let userData
  var dataToSend = [
   {
       "login": "login1",
       "money": 1000
   }
 ]
-
-fetch("./user_data.json")
+//fetch GET
+fetch("./new_user_data.json")
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => {
+    console.log(data);
+    userData = data;
+  })
   .catch(error => console.log(error))
-//     .then((response) => {
-//         data = response;
-//     })
-//     .catch(error => console.log(error))
-//     window.onload = () => console.log(data)
-
-// fetch("./user_data.json", {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json'
-//   },
-//   body: JSON.stringify(dataToSend)
-// })
-// .then((response) => {
-//   return response.json()
-// })
-// .then((response) => {
-//   data = response;
-//   console.log(data)
-// })
-// .catch(error => console.log(error))
-
+  //fetch POST
 fetch('/',{
   method: 'POST',
   headers: {
@@ -39,3 +21,28 @@ fetch('/',{
   },
   body: JSON.stringify(dataToSend)
 })
+
+
+const loadUser = () => {
+  document.querySelector('.money').innerHTML = userData[0].money;
+  document.querySelector('.money-per-click').innerHTML = userData[0].moneyPerClick;
+  document.querySelector('.money-per-auto').innerHTML = userData[0].moneyPerAuto;
+  document.querySelector('.value-multipler-bonus').innerHTML = userData[0].valueMultiplerBonus;
+  document.querySelector('.value-click-bonus').innerHTML = userData[0].valueClickBonus;
+  document.querySelector('.factories-bonus').innerHTML = userData[0].factoriesBonus;
+  document.querySelector('.workers-bonus').innerHTML = userData[0].workersBonus;
+  userData[0].autoMachine.forEach(machine =>{
+    document.querySelectorAll('.machine-cost')[machine.id -1].innerHTML = machine.cost
+  })
+
+
+
+
+
+
+  }
+document.querySelector('body').addEventListener('click', () => {
+  console.log(userData)
+  loadUser()
+})
+
