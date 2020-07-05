c476029174dd984584b4e260aed28f957cf360d6
Tomasz Bochnak <tomaszbochnak00@gmail.com>
2020-07-05 03:47:27 +0200

    changes

--- public/js/script-front.js
+++ public/js/script-front.js
@@ -24,26 +24,63 @@ fetch('/',{
 
 
 const loadUser = () => {
+  // main => show bonuses
   document.querySelector('.money').innerHTML = userData[0].money;
   document.querySelector('.money-per-click').innerHTML = userData[0].moneyPerClick;
   document.querySelector('.money-per-auto').innerHTML = userData[0].moneyPerAuto;
-  document.querySelector('.value-multipler-bonus').innerHTML = userData[0].valueMultiplerBonus;
-  document.querySelector('.value-click-bonus').innerHTML = userData[0].valueClickBonus;
-  document.querySelector('.factories-bonus').innerHTML = userData[0].factoriesBonus;
-  document.querySelector('.workers-bonus').innerHTML = userData[0].workersBonus;
-
+  // main => upgrades (four squares)
+  document.querySelector('.value-multipler-bonus').innerHTML = userData[0].valueMultiplerBonus.bonus;
+  document.querySelector('.upgrade-multipler-click-bonus-cost').innerHTML = userData[0].valueMultiplerBonus.upgradeCost;
+  document.querySelector('.value-click-bonus').innerHTML = userData[0].valueClickBonus.bonus;
+  document.querySelector('.upgrade-value-click-bonus-cost').innerHTML = userData[0].valueClickBonus.upgradeCost;
+  document.querySelector('.factories-bonus').innerHTML = userData[0].factoriesBonus.bonus;
+  document.querySelector('.upgrade-factories-bonus-cost').innerHTML = userData[0].factoriesBonus.upgradeCost;
+  document.querySelector('.workers-bonus').innerHTML = userData[0].workersBonus.bonus;
+  document.querySelector('.upgrade-workers-bonus-cost').innerHTML = userData[0].workersBonus.upgradeCost;
+  // main => machines and workers
   userData[0].autoMachine.forEach( machine => {
     document.querySelectorAll('.machine-cost')[machine.id -1].innerHTML = machine.cost
   });
   userData[0].workerMachine.forEach( worker => {
     document.querySelectorAll('.worker-cost')[worker.id -1].innerHTML = worker.cost
   });
+  //
+  
+  // tools => upgrades status
+  document.querySelector('.bonus-money-actual-level').innerHTML = userData[0].moneyBoost.actualLevel;
+  document.querySelector('.bonus-money-max-level').innerHTML = userData[0].moneyBoost.maxLevel;
+  document.querySelector('.bonus-money-cost').innerHTML = userData[0].moneyBoost.upgradeCost;
+  document.querySelector('.bonus-luck-actual-level').innerHTML = userData[0].moneyBoost.actualLevel;
+  document.querySelector('.bonus-luck-max-level').innerHTML = userData[0].moneyBoost.maxLevel;
+  document.querySelector('.luck-cost').innerHTML = userData[0].moneyBoost.upgradeCost;
+  document.querySelector('.bonus-tax-actual-level').innerHTML = userData[0].moneyBoost.actualLevel;
+  document.querySelector('.bonus-tax-max-level').innerHTML = userData[0].moneyBoost.maxLevel;
+  document.querySelector('.tax-cost').innerHTML = userData[0].moneyBoost.upgradeCost;
+  document.querySelector('.bonus-cooldown-actual-level').innerHTML = userData[0].moneyBoost.actualLevel;
+  document.querySelector('.bonus-cooldown-max-level').innerHTML = userData[0].moneyBoost.maxLevel;
+  document.querySelector('.cooldown-cost').innerHTML = userData[0].moneyBoost.upgradeCost;
+  document.querySelector('.luck-cost').innerHTML = userData[0].luck.upgradeCost;
+  document.querySelector('.tax-cost').innerHTML = userData[0].tax.upgradeCost;
+  document.querySelector('.cooldown-cost').innerHTML = userData[0].cooldown.upgradeCost;
+  // tools => upgrades skills
+  document.querySelector('.reset-skills-actual-level').innerHTML = userData[0].resetSkillsUpgrade.actualLevel
+  document.querySelector('.reset-skills-max-level').innerHTML = userData[0].resetSkillsUpgrade.maxLevel;
+  document.querySelector('.reset-skills-upgrade-cost').innerHTML = userData[0].resetSkillsUpgrade.upgradeCost;
+  document.querySelector('.multiple-click-skill-actual-level').innerHTML = userData[0].multipleClickSkillUpgrade.actualLevel
+  document.querySelector('.multiple-click-skill-max-level').innerHTML = userData[0].multipleClickSkillUpgrade.maxLevel;
+  document.querySelector('.multiple-click-skill-upgrade-cost').innerHTML = userData[0].multipleClickSkillUpgrade.upgradeCost;
+  document.querySelector('.multiple-money-skill-actual-level').innerHTML = userData[0].multipleMoneySkillUpgrade.actualLevel
+  document.querySelector('.multiple-money-skill-max-level').innerHTML = userData[0].multipleMoneySkillUpgrade.maxLevel;
+  document.querySelector('.multiple-money-skill-upgrade-cost').innerHTML = userData[0].multipleMoneySkillUpgrade.upgradeCost;
+  document.querySelector('.multiple-luck-skill-actual-level').innerHTML = userData[0].multipleLuckSkillUpgrade.actualLevel
+  document.querySelector('.multiple-luck-skill-max-level').innerHTML = userData[0].multipleLuckSkillUpgrade.maxLevel;
+  document.querySelector('.multiple-luck-skill-upgrade-cost').innerHTML = userData[0].multipleLuckSkillUpgrade.upgradeCost;
+  // tools => clickers
+  document.querySelector('.auto-clicker-cost').innerHTML = userData[0].autoClicker.cost;
+  document.querySelector('.auto-skills-cost').innerHTML = userData[0].autoSkills.cost;
 
 
-
-
-
-  }
+}
 document.querySelector('body').addEventListener('click', () => {
   console.log(userData)
   loadUser()
